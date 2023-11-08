029d3afa505a5e29d1e9d976451d20059c32677a
Noshin1234 <2020-1-60-221@std.ewubd.edu>
2023-11-08 21:21:15 +0600

    updated

--- sum.c
+++ sum.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 int main(){
-   int a,b,add,sub;
+   int a,b,add,sub,into;
     scanf("%d %d",&a,&b);
   
     add=a+b;
    sub=a-b;
-    
+    into=a*b;
     print("%f",add);
    printf("f",sub);
+   print("%f",into);
 }
