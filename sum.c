1ac534fc44a17984932b64252f02d4109324298d
Noshin1234 <2020-1-60-221@std.ewubd.edu>
2023-11-08 21:11:25 +0600

    added new info

--- sum.c
+++ sum.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 int main(){
-   int a,b,add,sub;
+   int a,b,add,sub,divition;
     scanf("%d %d",&a,&b);
   
     add=a+b;
    sub=a-b;
+divition=a/b;
     
     print("%f",add);
    printf("f",sub);
+      printf("f",division);
 }
